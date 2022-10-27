4c8f8d1d33461bee32f1de0c452bdae0ebab6e9e
oum-s <oumiyouss@hotmail.fr>
2022-10-27 17:26:22 +0200

    page Home faite

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,16 @@
 import './NotFound.scss';
+import { Link} from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div className="NotFound">
+    <div className="kasa-404">
       <h1>404</h1>
       <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      <p>Retourner sur la page d'accueil</p>
+
+      <Link to="/">
+        <p>Retourner sur la page d'accueil</p>
+      </Link>
+      
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,26 @@
 import './Header.scss';
+import logo from '../../assets/logo.png';
+import { NavLink} from 'react-router-dom';
 
 function Header() {
   return (
-    <div className="kasa-header">
+    <nav className="kasa-header">
+      <img className='logo' src={logo} alt='logo'></img>
       <ul>
         <li>
+        {/* utilisation d'une fonction react isActive */}
+        <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'link')}>
           Accueil
+        </NavLink>
         </li>
         <li>
+        <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'link')}>
           A propos
+          </NavLink>
         </li>
       </ul>
-    </div>
+      {/* <Outlet /> */}
+    </nav>
   );
 }
 
