2657ad487d8d784ae58a76e1c885eb508fada6be
oum-s <oumiyouss@hotmail.fr>
2022-10-28 18:49:57 +0200

    debut property page

--- src/Pages/Property/Property.js
+++ src/Pages/Property/Property.js
@@ -1,8 +1,11 @@
 import './Property.scss';
+import '../../components/Carousel/Carousel'
+import Carousel from '../../components/Carousel/Carousel';
 
 function Property() {
   return (
-    <div className="Property">
+    <div className="kasa-property">
+      <Carousel />
       <h1>Hey Property</h1>
     </div>
   );
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,7 +1,9 @@
 import '../Carousel/Carousel';
+// import Api from '../../api.json';
+
 function Carousel({CarouselImg}) {
   return <div className="kasa-Carousel">
-            <img src={CarouselImg} alt='Carousel-img'></img>
+            {/* <img src={CarouselImg} alt='Carousel-img'></img> */}
          </div>
 }
 //syntaxe prévue dans l'ES6 qui nous épargnera d'utiliser les accolades au moment de l'import
