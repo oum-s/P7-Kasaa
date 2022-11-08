b77f3690a80c10c267710e6b51dc27c77c213026
oum-s <oumiyouss@hotmail.fr>
2022-11-08 14:02:20 +0100

    css about

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -4,13 +4,22 @@ import PropTypes from "prop-types";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
 function Accordion ({title, content}) {
+  // state / état 
   const [isActive, setIsActive] = useState(false);
+  // affichage
   return (
     <div className="kasa-accordion__item">
+      {/* Au click, si isActive est false on la rend true et vice-versa */}
       <div className="kasa-accordion__title" onClick={() => setIsActive(!isActive)}>
+
         <div>{title}</div>
+
+        {/* si isActive est true on affichee le chevronUp et s'il est false on affiche le chevronDown */}
         <div>{isActive ? <FiChevronUp className='icon'/> : <FiChevronDown className='icon'/> }</div>
+
       </div>
+    
+      {/* quaad isActive est true on affiche le contenue */}
       {isActive && <div className="kasa-accordion__content">
         <ul>
           {content}
